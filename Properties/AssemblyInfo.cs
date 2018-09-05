7bd41b1457f355a7c67e5db115d4852f45382b92
Ebere Uzodufa <ebereuzodufa@gmail.com>
2018-09-05 11:36:27 +0100

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Color Coder")]
+[assembly: AssemblyTitle("Color Code")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
